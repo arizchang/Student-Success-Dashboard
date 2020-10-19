6f642d29e0ff15cdc8fd936cbfd9b19278255c96
Ariz Chang <abchang@asu.edu>
2020-10-18 17:14:05 -0700

    added starter code for server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,16 @@
 //starter file for the backend
+
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world!');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
