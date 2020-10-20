d0a10cd5735ffbe8f643f44662a2e3f25d65f932
Ariz Chang <abchang@asu.edu>
2020-10-19 21:46:45 -0700

    testing making http get requests

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
 //starter file for the backend
+//starter file
 
-const http = require('http');
+const { S_IFDIR } = require('constants')
+const http = require('http')
 
-const hostname = 'localhost';
-const port = 3000;
+const hostname = 'localhost'
+const port = 3000
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello world!');
-});
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'text/plain')
+	res.end('Hello world!')
+})
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}/`)
 })
---
+++ backend/serverRequest.js
@@ -0,0 +1,20 @@
+const http = require('http')
+
+//key: 3a672dc3e7a84eb490a11b47a1687683
+http.get(
+	'http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-20&sortBy=publishedAt&apiKey=3a672dc3e7a84eb490a11b47a1687683',
+	(resp) => {
+		let data = ''
+
+		// a chunk of data has been received
+		resp.on('data', (chunk) => {
+			data += chunk
+		})
+
+		//the whole response has been received. print the result
+		resp.on('end', () => {
+			let result = JSON.parse(data)
+			console.log(result.articles[0])
+		})
+	}
+)
