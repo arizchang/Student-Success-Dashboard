ed9d5832d34c9d331d802e1990f923a344c8bbc8
Ariz Chang <abchang@asu.edu>
2020-11-08 20:11:10 -0700

    added post, put, and delete requests

--- backend/axiosTest.js
+++ backend/axiosTest.js
@@ -1,14 +1,53 @@
-const { default: Axios } = require('axios')
-
 const axios = require('axios')
 
-function getTest() {
+//get requests
+function getTestLong() {
 	axios({
 		method: 'get',
 		url: 'https://jsonplaceholder.typicode.com/todos',
+		params: {
+			_limit: 5,
+		},
 	})
+		.then((res) => console.log(res.data))
+		.catch((err) => console.log(err))
+}
+
+function getTestShort() {
+	axios
+		.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+		.then((res) => console.log(res.data))
+		.catch((err) => console.log(err))
+}
+
+//post request (adding data)
+function postTest() {
+	axios
+		.post('https://jsonplaceholder.typicode.com/todos', {
+			title: 'New Todo',
+			completed: false,
+		})
 		.then((res) => console.log(res))
 		.catch((err) => console.log(err))
 }
 
-getTest()
+//put request (updating data)
+function putTest() {
+	axios
+		.put('https://jsonplaceholder.typicode.com/todos/1', {
+			title: 'Updated Todo',
+			completed: true,
+		})
+		.then((res) => console.log(res))
+		.catch((err) => console.log(err))
+}
+
+//delete request
+function deleteTest() {
+	axios
+		.delete('https://jsonplaceholder.typicode.com/todos/1')
+		.then((res) => console.log(res))
+		.catch((err) => console.log(err))
+}
+
+deleteTest()
