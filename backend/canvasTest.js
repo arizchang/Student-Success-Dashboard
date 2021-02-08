638b02f820cfb2cdca8b919426bbc9484b56477e
Ajai <ajai.gorowara@gmail.com>
2021-02-08 02:54:02 -0700

    Function to get all current classes and return a valid JSON

--- backend/canvasTest.js
+++ backend/canvasTest.js
@@ -1,6 +1,36 @@
 const axios = require('axios')
 const token = require('./tokens').token
 
+//Get all courses in the current year and term and return it as a JSON
+function getCurrentCourses() {
+	var term = "Spring";
+	var year = "2021";
+	var current = year.concat(term);
+	var myObj
+	var cur = []
+
+	axios
+		.get('https://asu.instructure.com/api/v1/courses?per_page=100', {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		.then((res) => {
+			myObj = res.data;
+			for(var i = 0; i < myObj.length; i++)
+			{
+				if(myObj[i]["course_code"].includes(current) == true){
+					cur.push(myObj[i])
+				}
+			}
+			//console.log(cur)
+			let json = JSON.stringify(cur);
+			//console.log(json);
+			return json;
+		})
+		.catch((err) => console.log(err))
+}
+
 function getCourses() {
 	axios
 		.get('https://asu.instructure.com/api/v1/courses?include=total_scores', {
@@ -60,8 +90,9 @@ function getAccount() {
 		.catch((err) => console.log(err))
 }
 
+getCurrentCourses()
 //getCourses()
 //getAssignments()
-getEnrollments()
+//getEnrollments()
 //getUser()
 //getAccount()
