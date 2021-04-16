1105107dbe398921878f7dd2b840288240d984c2
Ariz Chang <abchang@asu.edu>
2021-04-16 11:59:13 -0700

    fixed MyASU link in toolbar

--- frontend/Student-success-dashboard-new/src/pages/app/app.jsx
+++ frontend/Student-success-dashboard-new/src/pages/app/app.jsx
@@ -1,25 +1,25 @@
 import Route from '../../router'
-import { NavLink,Link,BrowserRouter } from 'react-router-dom';
-import './app.css';
+import { NavLink, Link, BrowserRouter } from 'react-router-dom'
+import './app.css'
 
 function App() {
-  return (
-    <BrowserRouter>
-    <div className="App">
-      <header className="App-header">
-        <nav>
-          <NavLink to='/home'>Home</NavLink>
-          <NavLink to='/calender'>Calender</NavLink>
-          <NavLink to='/dueDate'>Due Dates</NavLink>
-          <a href={'https://weblogin.asu.edu/cas/login'}>MyASU</a>
-        </nav>
-      </header>
-      <div className="content container">
-        <Route />
-      </div>
-    </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className='App'>
+				<header className='App-header'>
+					<nav>
+						<NavLink to='/home'>Home</NavLink>
+						<NavLink to='/calender'>Calender</NavLink>
+						<NavLink to='/dueDate'>Due Dates</NavLink>
+						<a href={'https://webapp4.asu.edu/myasu/'}>MyASU</a>
+					</nav>
+				</header>
+				<div className='content container'>
+					<Route />
+				</div>
+			</div>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
